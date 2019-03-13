12a67b1c47f02d3bb4e36ee434551e16e692a5ec
Reo Liu <liudev555@gmail.com>
2019-03-14 05:07:13 +0800

    New screen `Availability` added

---
+++ src/screens/availability/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyledText } from "../../components/text";
+import { ContainerView, View } from "../../components/views";
+
+const AvailabilityScreen = () => (
+  <ContainerView>
+    <View style={{ marginTop: 30 }}>
+      <StyledText>Availability Screen Coming Soon...</StyledText>
+    </View>
+  </ContainerView>
+);
+
+export default AvailabilityScreen;
