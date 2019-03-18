cf146478af2b83bbea1476673ed155af466326ce
Reo Liu <liudev555@gmail.com>
2019-03-19 00:20:56 +0800

    Refactored store using MobX-state-tree

--- src/store/app/index.js
+++ src/store/app/index.js
@@ -1,38 +1,17 @@
-import { observable } from "mobx";
-import { create, persist } from "mobx-persist";
-import { AsyncStorage } from "react-native";
+/* eslint-disable no-param-reassign */
+import { types } from "mobx-state-tree";
 
-class ApplicationState {
-  constructor() {
-    // TODO: make constructor valid
-    // eslint-disable-next-line no-console
-    console.log("Constructor");
-  }
-
-  /*
-    Our application global state.
-    It's observable, so can be updated with mobx.
-  */
-  @observable AppGlobalState = {
-    SplashShowing: true,
-    DemostrationVariable: false
-  };
-
-  @persist("object") @observable persistedUserData = {
-    name: "Some Data",
-    count: 0
-  };
-}
-
-const hydrate = create({
-  storage: AsyncStorage, // Choose our storage medium, ensure it's imported above
-  jsonify: true // if you use AsyncStorage, this needs to be true
-});
-
-const singleton = new ApplicationState();
-export default singleton;
-
-hydrate("persistedUserData", singleton).then(() => {
-  // eslint-disable-next-line no-console
-  console.log("Hydrated: persistedUserData");
-});
+export const ApplicationStore = types
+  .model("ApplicationStore", {
+    SplashShowing: types.boolean
+  })
+  .views(self => ({
+    getSplashShowing() {
+      return self.SplashShowing;
+    }
+  }))
+  .actions(self => ({
+    hideSplash() {
+      self.SplashShowing = false;
+    }
+  }));
--- src/store/provider/index.js
+++ src/store/provider/index.js
@@ -1,48 +1,24 @@
-import { observable, action } from "mobx";
-import { create, persist } from "mobx-persist";
-import { AsyncStorage } from "react-native";
+/* eslint-disable no-param-reassign */
+import { types } from "mobx-state-tree";
 
-class ProviderState {
-  @persist("object") @observable providerData = {
-    appointment: null,
-    readyProviders: null,
-    outstandingAppointment: null,
-    completeApplication: false
-  };
-
-  @action setAppointment(value) {
-    this.providerData.appointment = value;
-  }
-
-  @action setReadyProviders(value) {
-    this.providerData.readyProviders = value;
-  }
-
-  @action setOutstandingAppointment(value) {
-    this.providerData.outstandingAppointment = value;
-  }
-
-  @action setCompleteApplication(value) {
-    this.providerData.completeApplication = value;
-  }
-}
-
-const hydrate = create({
-  storage: AsyncStorage,
-  jsonify: true
-});
-
-const providerSingleton = new ProviderState();
-export default providerSingleton;
-
-hydrate("providerData", providerSingleton, {
-  providerData: {
-    appointment: false,
-    readyProviders: null,
-    outstandingAppointment: null,
-    completeApplication: false
-  }
-}).then(() => {
-  // eslint-disable-next-line no-console
-  console.log("Hydrated: providerData");
-});
+export const ProviderStore = types
+  .model("ProviderStore", {
+    appointment: types.boolean,
+    readyProviders: types.boolean,
+    outstandingAppointment: types.boolean,
+    completeApplication: types.boolean
+  })
+  .actions(self => ({
+    setAppointment(value) {
+      self.appointment = value;
+    },
+    setReadyProviders(value) {
+      self.readyProviders = value;
+    },
+    setOutstandingAppointment(value) {
+      self.outstandingAppointment = value;
+    },
+    setCompleteApplication(value) {
+      self.completeApplication = value;
+    }
+  }));
