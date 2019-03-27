21c5fdeb9bb4cca6cc882bfbf7f67d296ad7a24c
Reo Liu <liudev555@gmail.com>
2019-03-27 18:36:13 +0800

    Android and ios directory updated

--- android/app/src/main/java/com/careprovider/MainApplication.java
+++ android/app/src/main/java/com/careprovider/MainApplication.java
@@ -3,6 +3,11 @@ package com.careprovider;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.cardio.RNCardIOPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +27,12 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ReanimatedPackage(),
+            new MapsPackage(),
+            new RNGestureHandlerPackage(),
+            new RNCardIOPackage()
       );
     }
 
