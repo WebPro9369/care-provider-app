a95c4da3c27740cadde472b903d7b51df599d6a5
Reo Liu <liudev555@gmail.com>
2019-03-27 18:35:16 +0800

    Added index.js, removed splash screen in App.js

--- App.js
+++ App.js
@@ -1,29 +1,7 @@
 import React from "react";
-import styled from "styled-components/native";
-import { AppLoading, SplashScreen, Font } from "expo";
 import { Provider, observer } from "mobx-react";
-
 import { mainStore } from "./src/store";
 import RootContainer from "./src/root-container";
-import { DEVICE_WIDTH, DEVICE_HEIGHT } from "./src/utils/constants";
-
-const imgSplash = require("./assets/splash.png");
-
-const fontFlamaLight = require("./assets/fonts/FlamaLight_Regular.otf");
-const fontFlamaBasic = require("./assets/fonts/Flama_Regular.otf");
-const fontFlamaMedium = require("./assets/fonts/FlamaMedium_Regular.otf");
-
-const AppSplashWrapper = styled.View`
-  flex: 1;
-  width: ${DEVICE_WIDTH};
-  height: ${DEVICE_HEIGHT};
-`;
-
-const SplashImage = styled.Image`
-  flex: 1;
-  width: ${DEVICE_WIDTH};
-  height: ${DEVICE_HEIGHT};
-`;
 
 @observer
 export default class App extends React.Component {
@@ -41,13 +19,7 @@ export default class App extends React.Component {
 
   componentWillMount() {}
 
-  componentDidMount() {
-    Font.loadAsync({
-      "Flama-Light": fontFlamaLight,
-      "Flama-Basic": fontFlamaBasic,
-      "Flama-Medium": fontFlamaMedium
-    });
-  }
+  componentDidMount() {}
 
   componentWillReceiveProps() {}
 
@@ -62,31 +34,12 @@ export default class App extends React.Component {
     }, 5000);
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  onError() {}
-
-  // eslint-disable-next-line class-methods-use-this
-  onFinish() {
-    SplashScreen.hide();
-  }
-
   render() {
-    if (mainStore.applicationStore.getSplashShowing() === false) {
-      return (
-        <Provider store={mainStore}>
-          <RootContainer />
-        </Provider>
-      );
-    }
+    // if (mainStore.applicationStore.getSplashShowing() === false) {
     return (
-      <AppSplashWrapper>
-        <AppLoading
-          startAsync={this.onStartAsync}
-          onError={this.onError}
-          onFinish={this.onFinish}
-        />
-        <SplashImage resizeMode="cover" source={imgSplash} />
-      </AppSplashWrapper>
+      <Provider store={mainStore}>
+        <RootContainer />
+      </Provider>
     );
   }
 }
