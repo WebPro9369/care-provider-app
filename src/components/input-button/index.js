bf5f202ff56154403a79851a19164a6d38a7b3df
Reo Liu <liudev555@gmail.com>
2019-03-28 23:44:09 +0800

    New component InputButton added

---
+++ src/components/input-button/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Wrapper, TouchableButtonWrapper, ServiceText, Label } from "./styles";
+
+export const InputButton = ({ label, value, icon, onPress, ...rest }) => (
+  <Wrapper {...rest}>
+    <Label>{label}</Label>
+    <TouchableButtonWrapper onPress={onPress}>
+      <ServiceText>{value}</ServiceText>
+      {icon}
+    </TouchableButtonWrapper>
+  </Wrapper>
+);
+
+InputButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  icon: PropTypes.element,
+  onPress: PropTypes.func.isRequired
+};
+
+InputButton.defaultProps = {
+  icon: null
+};
---
+++ src/components/input-button/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components/native";
+import { colors } from "../../utils/constants";
+
+export const Wrapper = styled.View`
+  width: auto;
+  height: 56px;
+`;
+
+export const TouchableButtonWrapper = styled.TouchableOpacity`
+  background-color: ${colors.WHITE};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 9px;
+  padding-bottom: 9px;
+  border-style: solid;
+  border-bottom-width: 0.5px;
+  border-color: #000;
+`;
+
+export const Label = styled.Text`
+  font-family: "FlamaMedium";
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.6);
+`;
+
+export const ServiceText = styled.Text`
+  color: ${colors.BLACK};
+  font-family: "Flama";
+  font-size: 20px;
+  line-height: 24px;
+  text-align: left;
+`;
