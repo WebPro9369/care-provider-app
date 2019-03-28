5ad1840dade35b2209106b2891fed8ad5a0b97e5
Reo Liu <liudev555@gmail.com>
2019-03-28 23:48:22 +0800

    Added new routes in Account Settings

--- src/navigation/account.navigator.js
+++ src/navigation/account.navigator.js
@@ -1,10 +1,30 @@
 import { createStackNavigator } from "react-navigation";
 import AccountScreen from "../screens/account";
+import SettingsScreen from "../screens/account/settings";
+import EditNameScreen from "../screens/account/edit-name";
+import EditEmailScreen from "../screens/account/edit-email";
+import EditAddressScreen from "../screens/account/edit-address";
+import EditPhoneNumberScreen from "../screens/account/edit-phonenumber";
 
 const AccountNavigator = createStackNavigator(
   {
     AccountDefault: {
       screen: AccountScreen
+    },
+    AccountSettings: {
+      screen: SettingsScreen
+    },
+    AccountEditName: {
+      screen: EditNameScreen
+    },
+    AccountEditEmail: {
+      screen: EditEmailScreen
+    },
+    AccountEditAddress: {
+      screen: EditAddressScreen
+    },
+    AccountEditPhoneNumber: {
+      screen: EditPhoneNumberScreen
     }
   },
   {
