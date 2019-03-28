4ff8af993d2da8de3778e12e66e5d0454f97a9ed
Reo Liu <liudev555@gmail.com>
2019-03-28 23:47:16 +0800

    New screen Settings under Account

---
+++ src/screens/account/settings/index.js
@@ -0,0 +1,172 @@
+import React from "react";
+import { Avatar } from "react-native-elements";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { StyledText } from "../../../components/text";
+import { NavHeader } from "../../../components/nav-header";
+import { InputButton } from "../../../components/input-button";
+import { ServiceButton } from "../../../components/service-button";
+import {
+  ContainerView,
+  HeaderWrapper,
+  ViewCentered,
+  View
+} from "../../../components/views";
+import { ScrollView } from "../../../components/views/scroll-view";
+import { colors } from "../../../utils/constants";
+
+const { GREEN, MIDGREY } = colors;
+const imgDoctor = require("../../../../assets/images/Doctor.png");
+
+class SettingsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Michael Brown",
+      address: "22341 Justice Ave APT 725",
+      email: "michaelbrown@gmail.com",
+      phone: "(415) 123 - 4567"
+    };
+  }
+
+  render() {
+    const {
+      navigation: { navigate }
+    } = this.props;
+    const { name, address, email, phone } = this.state;
+    return (
+      <ContainerView>
+        <HeaderWrapper>
+          <NavHeader
+            title="Settings"
+            size="medium"
+            hasBackButton
+            onPressBackButton={() => navigate("AccountDefault")}
+          />
+        </HeaderWrapper>
+        <ScrollView>
+          <ViewCentered paddingTop={0}>
+            <Avatar
+              rounded
+              size={120}
+              source={imgDoctor}
+              showEditButton
+              editButton={{
+                containerStyle: {
+                  backgroundColor: GREEN,
+                  borderRadius: 12
+                },
+                size: 24
+              }}
+            />
+          </ViewCentered>
+          <View>
+            <StyledText fontSize={24}>Personal Information</StyledText>
+            <View style={{ padding: 16 }}>
+              <InputButton
+                label="Name"
+                value={name}
+                icon={
+                  <FontAwesome name="angle-right" size={24} color={MIDGREY} />
+                }
+                onPress={() => navigate("AccountEditName")}
+              />
+            </View>
+            <View style={{ padding: 16 }}>
+              <InputButton
+                label="Address"
+                value={address}
+                icon={
+                  <FontAwesome name="angle-right" size={24} color={MIDGREY} />
+                }
+                onPress={() => navigate("AccountEditAddress")}
+              />
+            </View>
+            <View style={{ padding: 16 }}>
+              <InputButton
+                label="Email"
+                value={email}
+                icon={
+                  <FontAwesome name="angle-right" size={24} color={MIDGREY} />
+                }
+                onPress={() => navigate("AccountEditEmail")}
+              />
+            </View>
+            <View style={{ padding: 16 }}>
+              <InputButton
+                label="Phone Number"
+                value={phone}
+                icon={
+                  <FontAwesome name="angle-right" size={24} color={MIDGREY} />
+                }
+                onPress={() => navigate("AccountEditPhoneNumber")}
+              />
+            </View>
+          </View>
+          <View style={{ marginTop: 32, marginBottom: 32 }}>
+            <ServiceButton
+              title="Log Out"
+              onPress={() => console.tron.log("Logout clicked")}
+            />
+          </View>
+          {/* <View>
+            <StyledText
+              fontFamily="FlamaMedium"
+              fontSize={24}
+              style={{ paddingTop: 24, paddingBottom: 16 }}
+            >
+              Edit Children
+            </StyledText>
+            <ContentButton>
+              <FlexView>
+                <Avatar rounded size={40} source={imgFoxLarge} />
+                <StyledText
+                  fontFamily="Flama"
+                  fontSize={16}
+                  style={{ marginLeft: 12 }}
+                >
+                  Benjamin
+                </StyledText>
+              </FlexView>
+              <StyledText fontFamily="Flama" fontSize={16}>
+                6 yrs
+              </StyledText>
+            </ContentButton>
+            <ContentButton>
+              <FlexView>
+                <Avatar rounded size={40} source={imgFoxLarge} />
+                <StyledText
+                  fontFamily="Flama"
+                  fontSize={16}
+                  style={{ marginLeft: 12 }}
+                >
+                  Audrey
+                </StyledText>
+              </FlexView>
+              <StyledText fontFamily="Flama" fontSize={16}>
+                8 yrs
+              </StyledText>
+            </ContentButton>
+            <ContentButton onPress={() => navigate("SettingsEditChild")}>
+              <FlexView>
+                <Avatar rounded size={40} source={imgFoxLarge} />
+                <StyledText
+                  fontFamily="Flama"
+                  fontSize={16}
+                  style={{ marginLeft: 12 }}
+                >
+                  Tara
+                </StyledText>
+              </FlexView>
+              <StyledText fontFamily="Flama" fontSize={16}>
+                12 yrs
+              </StyledText>
+            </ContentButton>
+          </View> */}
+        </ScrollView>
+      </ContainerView>
+    );
+  }
+}
+
+export default SettingsScreen;
