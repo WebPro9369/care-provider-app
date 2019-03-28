fd3e8036d4117d8013910c26dc74045656430ea3
Reo Liu <liudev555@gmail.com>
2019-03-29 00:29:55 +0800

    New screen EditBank added under Account

---
+++ src/screens/account/edit-bank/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { FormTextInput } from "../../../components/text";
+import { NavHeader } from "../../../components/nav-header";
+import { ServiceButton } from "../../../components/service-button";
+import { FormWrapper } from "../../../components/views";
+import {
+  KeyboardAvoidingView,
+  FormInputView
+} from "../../../components/views/keyboard-view";
+import { colors } from "../../../utils/constants";
+
+const { BLUE } = colors;
+
+class EditBankScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      accountNumber: "1234 5678 3456 2456",
+      routingNumber: "1234 5678 3456 2456"
+    };
+  }
+
+  render() {
+    const {
+      navigation: { goBack }
+    } = this.props;
+    const { accountNumber, routingNumber } = this.state;
+    return (
+      <KeyboardAvoidingView behavior="padding" enabled>
+        <NavHeader
+          title="Edit bank"
+          size="medium"
+          hasBackButton
+          onPressBackButton={() => goBack()}
+        />
+        <FormWrapper>
+          <FormInputView>
+            <FormTextInput
+              label="Account Number"
+              leftIcon={<FontAwesome name="cc-visa" size={30} color={BLUE} />}
+              value={accountNumber}
+            />
+          </FormInputView>
+          <FormInputView>
+            <FormTextInput label="Routing Number" value={routingNumber} />
+          </FormInputView>
+        </FormWrapper>
+        <ServiceButton title="Save Bank" onPress={() => goBack()} />
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+export default EditBankScreen;
