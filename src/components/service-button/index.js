bb5d5d33b6d3f41bf2f5afe4bd4850dfd09d541c
Reo Liu <liudev555@gmail.com>
2019-04-03 23:27:15 +0800

    Updated service button component to add `grey` prop

--- src/components/service-button/index.js
+++ src/components/service-button/index.js
@@ -1,22 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Wrapper, ServiceTouchableButtonWrapper, ServiceText } from "./styles";
+import { colors } from "../../utils/constants";
 
-export const ServiceButton = ({ title, icon, onPress, ...rest }) => (
+export const ServiceButton = ({ grey, title, icon, onPress, ...rest }) => (
   <Wrapper {...rest}>
-    <ServiceTouchableButtonWrapper onPress={onPress}>
-      <ServiceText>{title}</ServiceText>
+    <ServiceTouchableButtonWrapper grey={grey} onPress={onPress}>
+      <ServiceText color={grey ? colors.BLACK38 : colors.WHITE}>
+        {title}
+      </ServiceText>
     </ServiceTouchableButtonWrapper>
   </Wrapper>
 );
 
 ServiceButton.propTypes = {
+  grey: PropTypes.bool,
   title: PropTypes.string.isRequired,
   icon: PropTypes.element,
   onPress: PropTypes.func.isRequired
 };
 
 ServiceButton.defaultProps = {
+  grey: false,
   icon: null
 };
 
