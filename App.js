727eedcd0cce03e21b734eed62695e0041c53a83
Reo Liu <liudev555@gmail.com>
2019-04-18 00:48:22 +0900

    Added Twilio services

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { AppState } from "react-native";
 import { Provider, observer } from "mobx-react";
+import { TwilioService } from "./src/services";
 import { mainStore } from "./src/store";
 import RootContainer from "./src/root-container";
 
@@ -19,19 +21,50 @@ export default class App extends React.Component {
 
   componentWillMount() {}
 
-  componentDidMount() {}
+  componentDidMount() {
+    AppState.addEventListener("change", this.handleAppStateChange);
+  }
 
   componentWillReceiveProps() {}
 
   componentWillUpdate() {}
 
-  componentWillUnmount() {}
+  componentWillUnmount() {
+    AppState.removeEventListener("change", this.handleAppStateChange);
+  }
 
   // eslint-disable-next-line class-methods-use-this
   onStartAsync() {
     setTimeout(() => {
       mainStore.applicationStore.hideSplash();
     }, 5000);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleAppStateChange(appState) {
+    if (appState === "background") {
+      console.tron.log("Appstate changed: ", appState);
+      // let date = new Date(Date.now() + 10 * 1000);
+
+      TwilioService.sendNotification(
+        "Test Notification",
+        "This is a notification from my device!!!",
+        null,
+        "reo"
+      );
+
+      TwilioService.sendSMS("SMS Boby", null, "+19085008863");
+      TwilioService.makeCall(null, null, "+19085008863");
+
+      // if (Platform.OS === "ios") {
+      //   date = date.toISOString();
+      // }
+
+      // PushNotification.localNotificationSchedule({
+      //   message: "My Notification Message",
+      //   date
+      // });
+    }
   }
 
   render() {
