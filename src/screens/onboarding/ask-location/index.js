1bb22f540c048aae669b3911a63f4ce5a6d98d6b
Reo Liu <liudev555@gmail.com>
2019-04-24 00:51:41 +0900

    Get user location and save it to mobx state

--- src/screens/onboarding/ask-location/index.js
+++ src/screens/onboarding/ask-location/index.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import { Image, View } from "react-native";
+import { inject, observer, PropTypes } from "mobx-react";
+import axios from "axios";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Geolocation from "react-native-geolocation-service";
+import Geocoder from "react-native-geocoder";
 import { ServiceButton } from "../../../components/service-button";
 import { StyledText, StyledTextInput } from "../../../components/text";
 import { NavHeader } from "../../../components/nav-header";
 import { KeyboardAvoidingView } from "../../../components/views/keyboard-view";
-import { colors } from "../../../utils/constants";
+import { colors, GOOGLE_API_KEY } from "../../../utils/constants";
 
 const imgProgressbar = require("../../../../assets/images/ProgressBar1.png");
 
+@inject("store")
+@observer
 class AskLocationScreen extends Component {
+  static propTypes = {
+    store: PropTypes.observableObject.isRequired
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -17,16 +27,124 @@ class AskLocationScreen extends Component {
     };
   }
 
-  handleInputChange = event => {
+  componentDidMount() {
+    const {
+      store: {
+        providerStore: {
+          onboardingData: { address }
+        }
+      }
+    } = this.props;
+    Geolocation.getCurrentPosition(
+      position => {
+        console.tron.log(
+          "Current position: ",
+          position.coords.latitude,
+          position.coords.longitude
+        );
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        const ny = {
+          lat: 40.7809261,
+          lng: -73.9637594
+        };
+
+        const key = "AIzaSyBu1rXRtcQVBHRHotogui7F2FWT9WpfcNw";
+        // Geocoder.geocodePosition({
+        //   lat: position.coords.latitude,
+        //   lng: position.coords.longitude
+        // })
+        // Geocoder.geocodeAddress("New York")
+        //   .then(res => {
+        //     console.tron.log("Geocode: ", res);
+        //   })
+        //   .catch(err => {
+        //     console.tron.log("Error geocode: ", err);
+        // https://maps.googleapis.com/maps/api/js?key=AIzaSyBu1rXRtcQVBHRHotogui7F2FWT9WpfcNw
+        //   });
+        axios.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + ny.lat + "," + ny.lng + "&key=" + key)
+          .then(res => {
+            const addressComponents =
+              res.data &&
+              res.data.results[0] &&
+              res.data.results[0].address_components;
+            const formattedAddress =
+              res.data &&
+              res.data.results[0] &&
+              res.data.results[0].formatted_address;
+
+            console.tron.log(
+              "Google response: ",
+              res.data.results[0].address_components
+            );
+            if (addressComponents) {
+              addressComponents.forEach((item, index) => {
+                switch (item.types[0]) {
+                  case "street_number":
+                    address.setApartmentNumber(item.long_name);
+                    break;
+
+                  case "route":
+                    address.setStreet(item.long_name);
+                    break;
+
+                  case "postal_code":
+                    address.setZipCode(item.long_name);
+                    break;
+
+                  default:
+                    break;
+                }
+
+                if (item.types.includes("sublocality")) {
+                  address.setCity(item.long_name);
+                }
+                if (item.types.includes("locality")) {
+                  address.setState(item.long_name);
+                }
+              });
+            }
+
+            if (formattedAddress) {
+              address.setName(formattedAddress);
+            }
+
+            address.setLatitude(ny.lat.toString());
+            address.setLongitude(ny.lng.toString());
+          })
+          .catch(err => console.tron.log("Google error: ", err));
+      },
+      error => {
+        console.tron.log("Error current position: ", error);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }
+
+  handleInputChange = text => {
     this.setState({
-      zipcode: event.target.value
+      zipcode: text
     });
   };
 
-  render() {
+  onSubmit = () => {
     const {
-      navigation: { navigate }
+      navigation: { navigate },
+      store: {
+        providerStore: {
+          onboardingData: { address }
+        }
+      }
     } = this.props;
+    const { zipcode } = this.state;
+    console.tron.log("Location onsubmit");
+    if (zipcode) address.setZipCode(zipcode);
+    navigate("NameCapture");
+  };
+
+  render() {
     const { zipcode } = this.state;
     return (
       <KeyboardAvoidingView behavior="padding" enabled>
@@ -54,7 +172,7 @@ class AskLocationScreen extends Component {
               autoFocus
               placeholder="Zip code"
               value={zipcode}
-              onChange={this.handleInputChange}
+              onChangeText={this.handleInputChange}
             />
             <FontAwesome
               name="map-marker"
@@ -68,7 +186,7 @@ class AskLocationScreen extends Component {
           <ServiceButton
             title="Check Availability"
             style={{ marginBottom: 20 }}
-            onPress={() => navigate("NameCapture")}
+            onPress={this.onSubmit}
           />
         </View>
       </KeyboardAvoidingView>
