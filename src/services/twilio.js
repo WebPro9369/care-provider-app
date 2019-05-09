942b5d658909d272c47f4616db1017035c9fdb89
Reo Liu <liudev555@gmail.com>
2019-05-09 22:50:52 +0900

    Updated twilio service functions

--- src/services/twilio.js
+++ src/services/twilio.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 
 const TWILIO_API = "https://opear-twilio-api.herokuapp.com/";
+// const TWILIO_API = "http://192.168.3.211:8080/";
 const axios = Axios.create({
   baseURL: TWILIO_API,
   headers: {
@@ -10,8 +11,26 @@ const axios = Axios.create({
   }
 });
 
-const bindDevice = (token, bindingType = "apn") => {
-  axios
+const getToken = () => {
+  return axios
+    .post("/authtoken", {
+      ClientName: `tester${new Date().getTime()}`,
+      identity: `tester${new Date().getTime()}`
+    })
+    .then(token => {
+      console.tron.log("Get token; ", token);
+      return token;
+    })
+    .catch(err => console.tron.log("Error getting twilio token: ", err));
+};
+
+const bindDevice = (
+  token,
+  bindingType = "apn",
+  successHandler,
+  errorHandler
+) => {
+  return axios
     .post("/binding", {
       serviceName: "provider",
       identity: `user_${token}`,
@@ -21,15 +40,28 @@ const bindDevice = (token, bindingType = "apn") => {
     })
     .then(res => {
       console.tron.log("Twilio binding done: ", res);
+      if (successHandler) {
+        successHandler(res);
+      }
     })
     .catch(err => {
       console.tron.log("Twilio binding error: ", err);
+      if (errorHandler) {
+        errorHandler(err);
+      }
     });
 };
 
-const sendNotification = (title, body, identity, tag) => {
+const sendNotification = (
+  title,
+  body,
+  identity,
+  tag,
+  successHandler,
+  errorHandler
+) => {
   console.tron.log("Sending Twilio Notification: ", title, body, tag);
-  axios
+  return axios
     .post("/send-notification", {
       serviceName: "provider",
       title,
@@ -39,15 +71,21 @@ const sendNotification = (title, body, identity, tag) => {
     })
     .then(res => {
       console.tron.log("Twilio notify done: ", res);
+      if (successHandler) {
+        successHandler(res);
+      }
     })
     .catch(err => {
       console.tron.log("Twilio notify error: ", err);
+      if (errorHandler) {
+        errorHandler(err);
+      }
     });
 };
 
-const sendSMS = (body, from, to) => {
+const sendSMS = (body, from, to, successHandler, errorHandler) => {
   console.tron.log("Sending SMS: ", body, from, to);
-  axios
+  return axios
     .post("/send-sms", {
       body,
       from,
@@ -55,15 +93,21 @@ const sendSMS = (body, from, to) => {
     })
     .then(res => {
       console.tron.log("Twilio SMS Sent: ", res);
+      if (successHandler) {
+        successHandler(res);
+      }
     })
     .catch(err => {
       console.tron.log("Twilio SMS Error: ", err);
+      if (errorHandler) {
+        errorHandler(err);
+      }
     });
 };
 
-const makeCall = (url, from, to) => {
+const makeCall = (url, from, to, successHandler, errorHandler) => {
   console.tron.log("Making a call: ", url, from, to);
-  axios
+  return axios
     .post("/make-call", {
       url,
       from,
@@ -71,10 +115,16 @@ const makeCall = (url, from, to) => {
     })
     .then(res => {
       console.tron.log("Twilio call made: ", res);
+      if (successHandler) {
+        successHandler(res);
+      }
     })
     .catch(err => {
       console.tron.log("Twilio Call Error: ", err);
+      if (errorHandler) {
+        errorHandler(err);
+      }
     });
 };
 
-export default { bindDevice, sendNotification, sendSMS, makeCall };
+export default { getToken, bindDevice, sendNotification, sendSMS, makeCall };
