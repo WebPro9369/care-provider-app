00ff867fc092143f17d231ad8d8166e8f8c23ec7
Reo Liu <liudev555@gmail.com>
2019-05-30 23:05:01 +0900

    Google api key for matter

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,8 +3,7 @@ import { Dimensions } from "react-native";
 const DEVICE_WIDTH = Dimensions.get("window").width;
 const DEVICE_HEIGHT = Dimensions.get("window").height;
 
-// const GOOGLE_API_KEY = "AIzaSyBte8wMJ5Lic5CnLK8JRA8TniNwiCRpw7Q";
-const GOOGLE_API_KEY = "AIzaSyBu1rXRtcQVBHRHotogui7F2FWT9WpfcNw";
+const GOOGLE_API_KEY = "AIzaSyBte8wMJ5Lic5CnLK8JRA8TniNwiCRpw7Q";
 
 const colors = {
   WHITE: "#FFFFFF",
