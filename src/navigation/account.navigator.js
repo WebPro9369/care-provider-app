6dd433d5cb49066f08edc96ffbe95036fba50aa8
Brenton <brenton@Hackintosh.local>
2019-06-13 10:01:49 -0400

    added closed bracket to line 37 to fix syntax error

--- src/navigation/account.navigator.js
+++ src/navigation/account.navigator.js
@@ -34,6 +34,7 @@ const AccountNavigator = createStackNavigator(
     },
     AccountEditBio: {
       screen: EditBioScreen
+    },
     AccountEditSpecialties: {
       screen: EditSpecialtiesScreen
     },
