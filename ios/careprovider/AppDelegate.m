6692f3bb5aa681b649c3331060187977477ab353
Elijah Murray <elijahmurray@gmail.com>
2019-06-16 13:33:07 -0400

    link sentry to correct project

--- ios/careprovider/AppDelegate.m
+++ ios/careprovider/AppDelegate.m
@@ -27,7 +27,7 @@
 RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"careprovider"
                                             initialProperties:nil];
-  [RNSentry installWithRootView:rootView];
+[RNSentry installWithRootView:rootView];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
