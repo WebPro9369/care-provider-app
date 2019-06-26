6d8c0f172d44ec14303abf0371082f11a96121ab
Kyle Sloka-Frey <kyleslokafrey@Kyles-MacBook-Pro.local>
2019-06-26 16:26:45 -0400

    Revert "Fixed inactive user's from doing dashboard stuff"
    This reverts commit 80f0cff61d7adc1ba9df6cf454f626206d208625.

--- src/screens/auth/signin/index.js
+++ src/screens/auth/signin/index.js
@@ -69,15 +69,12 @@ class SignInScreen extends React.Component {
           work_history: workHistory,
           specialties,
           offered_services: offeredServices,
-          active,
           source,
           supervisor,
           stripe_balance,
           payout_account,
           dob: dateOfBirth
         } = res.data;
-
-        if (!active) return navigate("ApplicationPending");
 
         const dob = getFormattedDate(new Date(dateOfBirth));
 
