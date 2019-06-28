7b7f7327ee48d5b65f26e5a8753567be2284ffb2
Reo Liu <liudev555@gmail.com>
2019-06-28 10:28:23 +0900

    Test twilio notifications removed from the app

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
 import React from "react";
 import PushNotification from "react-native-push-notification";
-import { AppState } from "react-native";
 import { Provider, observer } from "mobx-react";
+import { Sentry } from "react-native-sentry";
 import { TwilioService } from "./src/services";
 import { mainStore } from "./src/store";
 import RootContainer from "./src/root-container";
 
-import { Sentry } from 'react-native-sentry';
-
-Sentry.config('https://cf2675d7f0cf4c97996e262a36d9bf34@sentry.io/1220229').install();
-
+Sentry.config(
+  "https://cf2675d7f0cf4c97996e262a36d9bf34@sentry.io/1220229"
+).install();
 
 @observer
 export default class App extends React.Component {
@@ -22,10 +23,10 @@ export default class App extends React.Component {
       console.log("Reactotron Configured")
     );
 
-    
     PushNotification.configure({
       onRegister({ token }) {
-        console.tron && console.tron.log("Push notification device token: ", token);
+        console.tron &&
+          console.tron.log("Push notification device token: ", token);
 
         const { currentUserStore } = mainStore;
         currentUserStore.setNotificationToken(token);
@@ -39,41 +40,11 @@ export default class App extends React.Component {
 
   componentWillMount() {}
 
-  componentDidMount() {
-    AppState.addEventListener("change", this.handleAppStateChange);
-  }
-
-  componentWillReceiveProps() {}
-
-  componentWillUpdate() {}
-
-  componentWillUnmount() {
-    AppState.removeEventListener("change", this.handleAppStateChange);
-  }
-
   // eslint-disable-next-line class-methods-use-this
   onStartAsync() {
     setTimeout(() => {
       mainStore.applicationStore.hideSplash();
     }, 5000);
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  handleAppStateChange(appState) {
-    if (appState === "background") {
-      console.tron.log("Appstate changed: ", appState);
-      // let date = new Date(Date.now() + 10 * 1000);
-
-      TwilioService.sendNotification(
-        "Test Notification",
-        "This is a notification from Twilio!!!",
-        null,
-        "reo"
-      );
-
-      TwilioService.sendSMS("SMS Boby", null, "+19085008863");
-      TwilioService.makeCall(null, null, "+19085008863");
-    }
   }
 
   render() {
