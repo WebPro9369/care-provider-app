681dce36021fe16d93114db2dbf002ec1d52aa47
Nick Pirollo <nsp37@drexel.edu>
2019-07-01 13:16:28 -0700

    Gracefully fail authLoad process

--- src/screens/authLoading/index.js
+++ src/screens/authLoading/index.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import { Alert, ActivityIndicator, View, Linking } from "react-native";
 import { inject, observer, PropTypes } from "mobx-react";
 import { getCareProvider } from "@services/opear-api";
-import { getAuthentication } from "@services/authentication";
+import { getAuthentication, removeAuthentication } from "@services/authentication";
 import { getFormattedDate } from "@utils/helpers";
 
 @inject("store")
@@ -51,122 +51,133 @@ class AuthLoadingScreen extends Component {
     const {
       navigation: { navigate }
     } = this.props;
-    const {
-      id,
-      apiKey,
-      isAuthenticated,
-      wasAuthenticated
-    } = await getAuthentication();
-
-    if (!isAuthenticated && wasAuthenticated) return navigate("Authenticating");
-    if (!isAuthenticated) return navigate("Onboarding");
-
-    const {
-      store: { currentUserStore }
-    } = this.props;
-    currentUserStore.setAuthentication({ id, apiKey });
-
-    const successHandler = res => {
+    try {
       const {
-        name,
-        email,
-        phone,
-        zip,
-        certification,
-        title: titles,
-        malpractice,
-        license_number: licenseNumber,
-        license_type: licenseType,
-        license_issuer: licenseIssuer,
-        license_country: licenseCountry,
-        license_state: licenseState,
-        license_city: licenseCity,
-        government_id_country: governmentIdCountry,
-        government_id_type: governmentIdType,
-        government_id_number: governmentIdNumber,
-        education,
-        work_history: workHistory,
-        specialties,
-        offered_services: offeredServices,
-        source,
-        supervisor,
-        stripe_balance,
-        payout_account,
-        dob: dateOfBirth,
-        active,
-        biography,
-        addresses,
-        rating
-      } = res.data;
+        id,
+        apiKey,
+        isAuthenticated,
+        wasAuthenticated
+      } = await getAuthentication();
 
-      if (!active) return navigate("ApplicationPending");
+      if (!isAuthenticated && wasAuthenticated)
+        return navigate("Authenticating");
+      if (!isAuthenticated) return navigate("Onboarding");
 
-      const dob = getFormattedDate(new Date(dateOfBirth));
+      const {
+        store: { currentUserStore }
+      } = this.props;
+      currentUserStore.setAuthentication({ id, apiKey });
 
-      const nameSplitted = (name || "").split(" ");
-      const firstName = nameSplitted[0];
-      const lastName =
-        nameSplitted.length > 1 ? nameSplitted.splice(1).join(" ") : "";
-      const { address, application } = currentUserStore;
+      const successHandler = res => {
+        try {
+          const {
+            name,
+            email,
+            phone,
+            zip,
+            certification,
+            title: titles,
+            malpractice,
+            license_number: licenseNumber,
+            license_type: licenseType,
+            license_issuer: licenseIssuer,
+            license_country: licenseCountry,
+            license_state: licenseState,
+            license_city: licenseCity,
+            government_id_country: governmentIdCountry,
+            government_id_type: governmentIdType,
+            government_id_number: governmentIdNumber,
+            education,
+            work_history: workHistory,
+            specialties,
+            offered_services: offeredServices,
+            source,
+            supervisor,
+            stripe_balance,
+            payout_account,
+            dob: dateOfBirth,
+            active,
+            biography,
+            addresses,
+            rating
+          } = res.data;
 
-      currentUserStore
-        .setFirstName(firstName)
-        .setLastName(lastName)
-        .setEmail(email)
-        .setPhone(phone)
-        .setStripeBalance(stripe_balance)
-        .setPayoutAccount(payout_account)
-        .setRating(rating);
+          if (!active) return navigate("ApplicationPending");
 
-      address.setZipCode(zip);
+          const dob = getFormattedDate(new Date(dateOfBirth));
 
-      if (addresses && addresses.length > 0) {
-        const addressData = addresses[addresses.length - 1];
-        address
-          .setName(addressData.name || "")
-          .setStreet(addressData.street || "")
-          .setCity(addressData.city || "")
-          .setState(addressData.state || "");
-      }
+          const nameSplitted = (name || "").split(" ");
+          const firstName = nameSplitted[0];
+          const lastName =
+            nameSplitted.length > 1 ? nameSplitted.splice(1).join(" ") : "";
+          const { address, application } = currentUserStore;
 
-      application
-        .setBoardCertification(certification)
-        .setTitles(titles)
-        .setMalpracticeInsurance(malpractice)
-        .setSupervisingPhysician(supervisor)
-        .setEducationHistory(education)
-        .setWorkHistory(workHistory)
-        .setSpecialties(specialties)
-        .setOfferedServices(offeredServices)
-        .setWhereHeard(source)
-        .setDateOfBirth(dob)
-        .setLicenseNumber(licenseNumber)
-        .setLicenseType(licenseType)
-        .setLicenseIssuer(licenseIssuer)
-        .setLicenseCountry(licenseCountry)
-        .setLicenseState(licenseState)
-        .setLicenseCity(licenseCity)
-        .setGovernmentIdType(governmentIdType)
-        .setGovernmentIdCountry(governmentIdCountry)
-        .setGovernmentIdNumber(governmentIdNumber)
-        .setBiography(biography);
+          currentUserStore
+            .setFirstName(firstName)
+            .setLastName(lastName)
+            .setEmail(email)
+            .setPhone(phone)
+            .setStripeBalance(stripe_balance)
+            .setPayoutAccount(payout_account)
+            .setRating(rating);
 
-      return navigate("Tabs");
-    };
+          address.setZipCode(zip);
 
-    const errorHandler = err => {
-      if (err.response.status === 401) {
-        navigate("Authenticating");
-      }
-      if (err.response.status === 500) {
-        Alert.alert(
-          "Error",
-          "There was an error signing in to your account. Please try again later"
-        );
-      }
-    };
+          if (addresses && addresses.length > 0) {
+            const addressData = addresses[addresses.length - 1];
+            address
+              .setName(addressData.name || "")
+              .setStreet(addressData.street || "")
+              .setCity(addressData.city || "")
+              .setState(addressData.state || "");
+          }
 
-    return getCareProvider(id, { successHandler, errorHandler });
+          application
+            .setBoardCertification(certification)
+            .setTitles(titles)
+            .setMalpracticeInsurance(malpractice)
+            .setSupervisingPhysician(supervisor)
+            .setEducationHistory(education)
+            .setWorkHistory(workHistory)
+            .setSpecialties(specialties)
+            .setOfferedServices(offeredServices)
+            .setWhereHeard(source)
+            .setDateOfBirth(dob)
+            .setLicenseNumber(licenseNumber)
+            .setLicenseType(licenseType)
+            .setLicenseIssuer(licenseIssuer)
+            .setLicenseCountry(licenseCountry)
+            .setLicenseState(licenseState)
+            .setLicenseCity(licenseCity)
+            .setGovernmentIdType(governmentIdType)
+            .setGovernmentIdCountry(governmentIdCountry)
+            .setGovernmentIdNumber(governmentIdNumber)
+            .setBiography(biography);
+
+          return navigate("Tabs");
+        } catch {
+          removeAuthentication();
+          return navigate("Authenticating");
+        }
+      };
+
+      const errorHandler = err => {
+        if (err.response.status === 401) {
+          navigate("Authenticating");
+        }
+        if (err.response.status === 500) {
+          Alert.alert(
+            "Error",
+            "There was an error signing in to your account. Please try again later"
+          );
+        }
+      };
+
+      return getCareProvider(id, { successHandler, errorHandler });
+    } catch {
+      removeAuthentication();
+      return navigate("Authenticating");
+    }
   };
 
   render() {
